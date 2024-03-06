7f84ddf487ec682f8f5081016902b5b7e6d17804
AnochAno <anothankes876@gmail.com>
2024-03-06 11:43:20 +0530

    backend

---
+++ BackEnd/src/test/java/com/Anojan/CarRenterService/CarRenterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anojan.CarRenterService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRenterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
